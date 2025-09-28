a48f6e12901a5ec652ddd9266729049c42289b0c
emdevelopa <gyimahsunday@gmail.com>
2025-09-28 08:59:09 +0100

    updates

--- loops.py
+++ loops.py
@@ -1,5 +1,9 @@
 # while n is 1,2,3...,10 print hello otherwise n is above 10 break loop
-n = 0
-while n <= 10:
-    n+=1
+# n = 0
+# while n <= 10:
+#     n+=1
+#     print("hello")
+
+n = 4
+for n in range(4):
     print("hello")
