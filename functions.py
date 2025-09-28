b402f66866f7d54323abe8e36ee2ef21877809f6
emdevelopa <gyimahsunday@gmail.com>
2025-09-28 08:56:38 +0100

    updates

---
+++ functions.py
@@ -0,0 +1,12 @@
+# def testing(args):
+#     print(f"this is a {args}")
+
+# testing("funtion")
+
+# while n is 1,2,3...,10 print hello otherwise n is above 10 break loop
+n = 0
+while n <= 10:
+    n+=1
+    print("hello")
+
+
