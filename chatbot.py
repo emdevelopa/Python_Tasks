3433731a5e24da8b17d07ca1a442ada33ef43d6f
emdevelopa <gyimahsunday@gmail.com>
2025-10-04 12:28:40 +0100

    updated code

---
+++ chatbot.py
@@ -0,0 +1,26 @@
+print("Hello I am ChatPy")
+print("type 'bye' to end the chat ")
+print("Enter 'h' to show help \n")
+
+while True:
+    user_input = input("You: ").lower()
+    
+    if user_input == "hello" or user_input == "hi":
+        print("ChatPy: Hey there! How can I help you today?")
+
+    elif "h" == user_input:
+        print(""" 
+        commands    hello/hi
+                    how are you
+                    your name
+                    bye
+        """)
+    elif "how are you" in user_input:
+        print("ChatPy: I'm doing great thanks for asking!")
+    elif "your name" in user_input:
+        print("ChatPy: I'm ChatPy, your python chatbot")
+    elif "bye" in user_input:
+        print("ChatPy: Goodbye! Have a great day!")
+        break
+    else:
+        print("ChatPy: Sorry, I don't understand that yet")
